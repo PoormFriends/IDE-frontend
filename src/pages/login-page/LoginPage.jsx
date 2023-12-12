720fb6f9a5cbbd69f846d2abaa8c61da395f507f
swimmingRiver <swimmingr@gmail.com>
2023-12-12 21:44:22 +0900

    design:kakao-login-button

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import kakaobutton from "../images/kakao_login_medium_wide.png";
 import styles from "./LoginPage.module.css";
 
 export default function LoginPage() {
@@ -6,31 +7,30 @@ export default function LoginPage() {
   const KakaRedirectUri = process.env.REACT_APP_KAKAO_REDIRECT_URI;
   const GithubRestApi = process.env.REACT_APP_GITHUB_API;
   const GithubRedirectUri = process.env.REACT_APP_GITHUB_REDIRECT_URI;
-
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code);
+  }, []);
   const kakaoLoginHandler = () => {
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KakaoRestApi}&redirect_uri=${KakaRedirectUri}&response_type=code`;
-    // const code = new URL(window.location.href).searchParams.get("code");
   };
   const githubLoginHandler = () => {
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${GithubRestApi}&redirect_uri=${GithubRedirectUri}&response_type=code`;
   };
+
   return (
     <div>
       <section className={styles["login-container"]}>
         <h2 className={styles["login-title"]}>Goorm Friends IDE</h2>
         <section className={styles["btn-container"]}>
           <button
-            onClick={kakaoLoginHandler}
             className={styles["btn-kakao"]}
             type="button"
+            onClick={kakaoLoginHandler}
           >
-            <img
-              className={styles["logo-kakao"]}
-              alt="kakao"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Kakao_Corp._symbol_-_2012.svg/100px-Kakao_Corp._symbol_-_2012.svg.png?20151013114543"
-            />
-            카카오로 로그인하기
+            <img alt="kakao-btn" src={kakaobutton} />
           </button>
+
           <button
             onClick={githubLoginHandler}
             className={styles["btn-github"]}
