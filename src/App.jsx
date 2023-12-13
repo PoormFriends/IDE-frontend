13c8dd81c0650450f7a79b799f143d037a994506
LeeDahee <dduosy9@gmail.com>
2023-12-14 04:42:25 +0900

    feat(IdePage): create Ide page sturcture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import IdePage from "./pages/ide/IdePage";
 
 function App() {
-  return <div> </div>;
+  return (
+    <div>
+      <IdePage />
+    </div>
+  );
 }
 
 export default App;
