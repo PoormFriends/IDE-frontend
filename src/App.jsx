b40c00c816702e39e49076e846c2db55b85a4ab6
swimmingRiver <swimmingr@gmail.com>
2023-12-16 03:16:47 +0900

    feat : add route redirectPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login-page/LoginPage";
+import RedirectPage from "./pages/login-page/RedirectPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/oauth2" element={<RedirectPage />} />
       </Routes>
     </Router>
   );
