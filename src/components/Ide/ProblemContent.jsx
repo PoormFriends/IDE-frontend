0ce49459d081009e8f226d39afda0a8f30664aa8
LeeDahee <dduosy9@gmail.com>
2023-12-17 02:04:19 +0900

    style: css 클래스명 수정

--- src/components/Ide/ProblemContent.jsx
+++ src/components/Ide/ProblemContent.jsx
@@ -4,9 +4,9 @@ import styles from "./ProblemContent.module.css";
 
 export default function ProblemContent({ type, content }) {
   return (
-    <article className={`${styles.problemInfo} ${styles.borderBottom}`}>
-      <h4 className={styles.problemInformationLabel}>{type}</h4>
-      <p className={styles.problemInformationContent}>{content}</p>
+    <article className={`${styles.container} ${styles.borderBottom}`}>
+      <h4 className={styles.label}>{type}</h4>
+      <p className={styles.contents}>{content}</p>
     </article>
   );
 }
