b7a50f435cfb1ffeb9df86472507f7af97fedcf1
LeeDahee <dduosy9@gmail.com>
2023-12-17 02:34:31 +0900

    feat: monaco editor 입력 값 가져오기

--- src/components/Ide/MonacoEditor.jsx
+++ src/components/Ide/MonacoEditor.jsx
@@ -1,21 +1,40 @@
-import React from "react";
-import { Editor } from "@monaco-editor/react";
+import React, { useRef } from "react";
+import Editor from "@monaco-editor/react";
 
 export default function MonacoEditor() {
+  const editorRef = useRef(null);
+
+  const handleEditorDidMount = editor => {
+    editorRef.current = editor;
+  };
+
+  function showValue() {
+    if (editorRef.current) {
+      alert(editorRef.current.getValue());
+    } else {
+      console.log("Editor is not mounted yet");
+    }
+  }
   return (
-    <Editor
-      defaultLanguage="java"
-      width="100%"
-      height="100%"
-      options={{
-        padding: { top: 15, bottom: 15 },
-        fontSize: 13,
-        minimap: { enabled: false },
-        scrollbar: {
-          vertical: "auto",
-          horizontal: "auto",
-        },
-      }}
-    />
+    <>
+      <Editor
+        defaultLanguage="java"
+        width="100%"
+        height="60vh"
+        options={{
+          padding: { top: 15, bottom: 15 },
+          fontSize: 13,
+          minimap: { enabled: false },
+          scrollbar: {
+            vertical: "auto",
+            horizontal: "auto",
+          },
+        }}
+        onMount={handleEditorDidMount}
+      />
+      <button type="button" onClick={showValue}>
+        show value
+      </button>
+    </>
   );
 }
