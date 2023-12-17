215d657fc86ba0d7f8729eadb6fe8f846bfd8c86
LeeDahee <dduosy9@gmail.com>
2023-12-17 19:04:14 +0900

    style: 공백제거

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import IdePage from "./pages/ide/IdePage";
 import LoginPage from "./pages/login-page/LoginPage";
 import RedirectPage from "./pages/login-page/RedirectPage";
 
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ProblemListsPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/oauth2" element={<RedirectPage />} /> 
+        <Route path="/oauth2" element={<RedirectPage />} />
         <Route path="/solve/:user_id/:problem_id" element={<IdePage />} />
       </Routes>
     </Router>
