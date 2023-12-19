7fce6e8251104d13f1df0289cc390ab607a74e2f
LeeDahee <dduosy9@gmail.com>
2023-12-20 03:25:04 +0900

    feat: query Client, queryClientProvider 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ProblemListsPage from "./pages/problem_lists_page/ProblemListsPage";
 import IdePage from "./pages/ide/IdePage";
 import LoginPage from "./pages/login-page/LoginPage";
 import RedirectPage from "./pages/login-page/RedirectPage";
+import MyListContainer from "./components/myList/MyListContainer";
 import { EditorProvider } from "./contexts/EditorContext";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <EditorProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<ProblemListsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/oauth2" element={<RedirectPage />} />
-          <Route path="/solve/:user_id/:problem_id" element={<IdePage />} />
-        </Routes>
-      </Router>
-    </EditorProvider>
+    <QueryClientProvider client={queryClient}>
+      <EditorProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProblemListsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/oauth2" element={<RedirectPage />} />
+            <Route path="/solve/:user_id/:problem_id" element={<IdePage />} />
+            <Route
+              path="/mylist/:userId/:problemId"
+              element={<MyListContainer />}
+            />
+          </Routes>
+        </Router>
+      </EditorProvider>
+    </QueryClientProvider>
   );
 }
 
