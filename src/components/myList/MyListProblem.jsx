cdd75e29687ab81b8b27e039b63367aea938711e
LeeDahee <dduosy9@gmail.com>
2023-12-20 00:07:26 +0900

    feat: 마이리스트 문제 삭제하기 fetch 함수 작성

--- src/components/myList/MyListProblem.jsx
+++ src/components/myList/MyListProblem.jsx
@@ -4,6 +4,25 @@ import { FaTrashAlt } from "react-icons/fa";
 import styles from "./MyListProblem.module.css";
 
 export default function MyListProblem({ title, level }) {
+  const handleDeleteProblem = () => {
+    fetch("http://localhost:8080/directory/problem", {
+      method: "DELETE",
+      header: {
+        // Authorization: Bearer 수정 예정
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // react-query로 전역상태 관리 수정예정
+        // userId,
+        // directory,
+        // problemId,
+        // directoryProblemId,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  };
   return (
     <div className={styles.container}>
       <p className={styles.title}>{title}</p>
@@ -12,6 +31,7 @@ export default function MyListProblem({ title, level }) {
         className={styles.deleteButton}
         type="button"
         aria-label="trash button"
+        onClick={handleDeleteProblem}
       >
         <FaTrashAlt />
       </button>
