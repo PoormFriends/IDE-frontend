a7500fcd97b1a5c01199d39ee0e2a7ab54811e02
LeeDahee <dduosy9@gmail.com>
2023-12-19 23:56:11 +0900

    feat: 리스트 삭제버튼 생성(html, css)

--- src/components/myList/MyList.jsx
+++ src/components/myList/MyList.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { useParams } from "react-router-dom";
+import { FaCircleMinus } from "react-icons/fa6";
+import { Tooltip } from "@mui/material";
 import MyListProblem from "./MyListProblem";
 import styles from "./MyList.module.css";
 
@@ -25,7 +27,18 @@ export default function MyList({ id, title, list }) {
   };
   return (
     <div className={styles.container}>
-      <h4 className={styles.title}>{title}</h4>
+      <div className={styles.header}>
+        <h4 className={styles.title}>{title}</h4>
+        <Tooltip title="리스트를 삭제합니다">
+          <button
+            className={styles.deleteButton}
+            type="button"
+            aria-label="리스트 삭제하기 버튼"
+          >
+            <FaCircleMinus className={styles.deleteIcon} />
+          </button>
+        </Tooltip>
+      </div>
       {/* 문제들 */}
       <div className={styles.problems}>
         {list &&
