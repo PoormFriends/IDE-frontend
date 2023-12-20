34026002bbc15bc4f745f6ae5224eb20d6046586
LeeDahee <dduosy9@gmail.com>
2023-12-21 08:39:06 +0900

    fix: 마이리스트 api 통신 예외처리 & 테스트 코드

--- src/api/MyListService.js
+++ src/api/MyListService.js
@@ -14,9 +14,7 @@ export const fetchMyLists = async userId => {
     console.error("서버 요청에 실패했습니다: ", error);
 
     // test: 서버 요청이 실패했을 때 모의 데이터 반환
-    return Promise.resolve([
-      { directoryId: "1", directoryName: "List 1", problemList: [] },
-    ]);
+    return [{ directoryId: "1", directoryName: "List 1", problemList: [] }];
 
     // 실제로는 test 부분 삭제하고 아래 코드 주석 해제
     // throw error;
@@ -25,50 +23,89 @@ export const fetchMyLists = async userId => {
 
 // 마이리스트 추가
 export const fetchAddMyList = async (userId, directoryTitle) => {
-  const response = await fetch(`https://localhost:8080/directory`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Authorization': Bearer accessToken
-    },
-    body: JSON.stringify({ userId, directoryTitle }),
-  });
-  if (!response.ok) {
-    throw new Error("마이리스트 추가 실패");
+  try {
+    console.log("fetchAddMyList 호출됨", userId, directoryTitle);
+    const response = await fetch(`https://localhost:8080/directory`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Authorization': Bearer accessToken
+      },
+      body: JSON.stringify({ userId, directoryTitle }),
+    });
+    if (!response.ok) {
+      throw new Error("마이리스트 추가 실패");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("fetchAddMyList 서버 요청에 실패했습니다: ", error);
+
+    // test: 서버 요청 실패해도 성공 결과 반환
+    return {
+      success: true,
+      message: "[test] 목록 추가 성공",
+    };
+    // 실제로는 test 부분 삭제하고 아래 코드 주석 해제
+    // throw error;
   }
-  return response.json();
 };
 
 // 마이리스트 삭제
 export const fetchDeleteMyList = async (userId, directoryId) => {
-  const response = await fetch(`http://localhost:8080/directory`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Authorization': Bearer accessToken
-    },
-    body: JSON.stringify({ userId, directoryId }),
-  });
-  if (!response.ok) {
-    throw new Error("마이리스트 삭제 실패");
+  try {
+    console.log("fetchDeleteMyList 호출됨", userId, directoryId);
+    const response = await fetch(`http://localhost:8080/directory`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Authorization': Bearer accessToken
+      },
+      body: JSON.stringify({ userId, directoryId }),
+    });
+    if (!response.ok) {
+      throw new Error("마이리스트 삭제 실패");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("fetchDeleteMyList 서버 요청에 실패했습니다: ", error);
+
+    // test: 서버 요청 실패해도 성공 결과 반환
+    return {
+      success: true,
+      message: "[test] 목록 삭제 성공",
+    };
+    // 실제로는 test 부분 삭제하고 아래 코드 주석 해제
+    // throw error;
   }
-  return response.json();
 };
 
 // 마이리스트 문제 추가
 export const fetchAddMyListProblem = async (userId, directoryId, problemId) => {
-  const response = await fetch(`http://localhost:8080/directory/problem`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Authorization': Bearer accessToken
-    },
-    body: JSON.stringify({ userId, directoryId, problemId }),
-  });
-  if (!response.ok) {
-    throw new Error("마이리스트 문제 추가 실패");
+  try {
+    console.log("fetchAddMyListProblem 호출됨", userId, directoryId, problemId);
+    const response = await fetch(`http://localhost:8080/directory/problem`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Authorization': Bearer accessToken
+      },
+      body: JSON.stringify({ userId, directoryId, problemId }),
+    });
+    if (!response.ok) {
+      throw new Error("마이리스트 문제 추가 실패");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("fetchAddMyListProblem 서버 요청에 실패했습니다: ", error);
+
+    // test: 서버 요청 실패해도 성공 결과 반환
+    return {
+      success: true,
+      message: "[test] 목록 삭제 성공",
+    };
+    // 실제로는 test 부분 삭제하고 아래 코드 주석 해제
+    // throw error;
   }
-  return response.json();
 };
 
 // 마이리스트 문제 삭제
@@ -78,21 +115,40 @@ export const fetchDeleteMyListProblem = async (
   problemId,
   directoryProblemId,
 ) => {
-  const response = await fetch(`http://localhost:8080/directory/problem`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Authorization': Bearer accessToken
-    },
-    body: JSON.stringify({
+  try {
+    console.log(
+      "fetchDeleteMyListProblem 호출됨",
       userId,
       directoryId,
       problemId,
       directoryProblemId,
-    }),
-  });
-  if (!response.ok) {
-    throw new Error("마이리스트 문제 삭제 실패");
+    );
+    const response = await fetch(`http://localhost:8080/directory/problem`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Authorization': Bearer accessToken
+      },
+      body: JSON.stringify({
+        userId,
+        directoryId,
+        problemId,
+        directoryProblemId,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("마이리스트 문제 삭제 실패");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("fetchDeleteMyListProblem 서버 요청에 실패했습니다: ", error);
+
+    // test: 서버 요청 실패해도 성공 결과 반환
+    return {
+      success: true,
+      message: "[test] 목록 삭제 성공",
+    };
+    // 실제로는 test 부분 삭제하고 아래 코드 주석 해제
+    // throw error;
   }
-  return response.json();
 };
