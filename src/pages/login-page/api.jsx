6a0635f65ba9cc5862441b093178c9be9723d23e
LeeDahee <dduosy9@gmail.com>
2023-12-21 23:37:07 +0900

    fix(app): accessToken 오류 해결

--- src/pages/login-page/api.jsx
+++ src/pages/login-page/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "https://localhost:8080",
+  baseURL: "https://localhost:8081",
 });
 export default instance;
 
