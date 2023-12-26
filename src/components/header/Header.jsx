95fd0fd40328fcb28e944f8b253891322315eed0
LeeDahee <dduosy9@gmail.com>
2023-12-26 23:57:29 +0900

    design(Header): height 조절, tooltip 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import Tooltip from "@mui/material/Tooltip";
 import styles from "./Header.module.css";
 import titleLogo from "../../assets/images/title_logo.png";
 import { requestLogout } from "../../pages/login-page/api";
@@ -8,30 +9,36 @@ import { requestLogout } from "../../pages/login-page/api";
 function Header() {
   const user = JSON.parse(localStorage.getItem("user"));
   return (
-    <div className={styles.header}>
-      <div className={styles.header_title}>
-        <Link to="/">
-          <img
-            src={titleLogo}
-            className={styles.header_title_icon}
-            alt="header-title-icon"
-          />
-        </Link>
+    <header className={styles.header}>
+      <Link to="/">
+        <img
+          src={titleLogo}
+          className={styles.logo_icon}
+          alt="header-title-icon"
+        />
+      </Link>
+      <div className={styles.button_container}>
+        <Tooltip title="마이페이지">
+          <Link to="/mypage">
+            <img
+              className={styles.mypage_img}
+              src={user?.profileImage}
+              alt="user-profile"
+            />
+          </Link>
+        </Tooltip>
+        <Tooltip title="Log out">
+          <button
+            type="button"
+            onClick={requestLogout}
+            className={styles.logout_button}
+            aria-label="logout button"
+          >
+            <FiLogOut className={styles.logout_icon} />
+          </button>
+        </Tooltip>
       </div>
-      <div className={styles.buttons}>
-        <Link to="/mypage">
-          <img
-            className={styles.header_user_profile}
-            src={user?.profileImage}
-            alt="user-profile"
-          />
-        </Link>
-
-        <div>
-          <FiLogOut onClick={requestLogout} className={styles.logout_icon} />
-        </div>
-      </div>
-    </div>
+    </header>
   );
 }
 
