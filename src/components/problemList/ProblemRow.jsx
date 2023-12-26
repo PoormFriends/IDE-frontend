2ab8e591315e4feffc98b2686e8137ee03bc5589
LeeDahee <dduosy9@gmail.com>
2023-12-27 03:17:04 +0900

    design: 제목 width 조정, 마이리스트 overflow 조절

--- src/components/problemList/ProblemRow.jsx
+++ src/components/problemList/ProblemRow.jsx
@@ -1,4 +1,4 @@
-import { Chip, TableCell, TableRow } from "@mui/material";
+import { Chip, TableCell, TableRow, Tooltip } from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
@@ -25,13 +25,13 @@ export default function ProblemRow({
   };
 
   return (
-    <TableRow className={styles.table_row}>
-      <TableCell align="center">
-        {state === "SUCCESS" && <FaRegCircle />}
-        {state === "FAILURE" && <IoMdClose />}
+    <TableRow className={styles.table_row} sx={{ paddingRight: 10 }}>
+      <TableCell align="center" sx={{ maxWidth: 50 }}>
+        {state === "SUCCESS" && <FaRegCircle style={{ color: "green" }} />}
+        {state === "FAILURE" && <IoMdClose style={{ color: "red" }} />}
       </TableCell>
       <TableCell align="center">{problemId}</TableCell>
-      <TableCell align="left">
+      <TableCell align="left" sx={{ minWidth: 300 }}>
         <Link to={`/solve/${userId}/${problemId}`} className={styles.link}>
           {problemName}
         </Link>
@@ -39,42 +39,53 @@ export default function ProblemRow({
       <TableCell align="center">
         <span className={styles.level}>{`Lv.${level}`}</span>
       </TableCell>
-      <TableCell align="center" onClick={toggleMyListModal}>
-        <div className={styles.mylist_container}>
-          {directories &&
-            directories.map(directory => (
-              <Chip
-                key={directory.customDirectoryId}
-                label={`${directory.customDirectoryName}`}
-                variant="outlined"
-                sx={{
-                  "--Chip-radius": "4px",
-                }}
-              />
-            ))}
-        </div>
-        {isMiniMyListVisible && (
-          <div
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: "rgba(0, 0, 0, 0.2)",
-              zIndex: 999,
-            }}
-          >
-            <MiniMyList
-              userId={userId}
-              currentMyLists={directories}
-              totalMyLists={totalMyLists}
-              problemId={problemId}
-              onClose={() => setIsMiniMyListVisible(false)}
-            />
+      <Tooltip title="마이리스트에 추가/삭제하기" placement="right">
+        <TableCell
+          sx={{
+            maxWidth: 300,
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+          }}
+          align="center"
+          onClick={toggleMyListModal}
+        >
+          <div className={styles.mylist_container}>
+            {directories &&
+              directories.map(directory => (
+                <Chip
+                  key={directory.customDirectoryId}
+                  label={`${directory.customDirectoryName}`}
+                  variant="outlined"
+                  sx={{
+                    "--Chip-radius": "4px",
+                  }}
+                />
+              ))}
           </div>
-        )}
-      </TableCell>
+          {isMiniMyListVisible && (
+            <div
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+                zIndex: 999,
+              }}
+            >
+              <MiniMyList
+                userId={userId}
+                currentMyLists={directories}
+                totalMyLists={totalMyLists}
+                problemId={problemId}
+                onClose={() => setIsMiniMyListVisible(false)}
+              />
+            </div>
+          )}
+        </TableCell>
+      </Tooltip>
     </TableRow>
   );
 }
