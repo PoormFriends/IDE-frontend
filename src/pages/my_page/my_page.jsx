e14a8a8565e04fe81b63da01362a8f9a759e5d11
LeeDahee <dduosy9@gmail.com>
2023-12-28 07:44:16 +0900

    design: 마이페이지 layout

--- src/pages/my_page/my_page.jsx
+++ src/pages/my_page/my_page.jsx
@@ -33,11 +33,11 @@ function MyPage() {
   }, []);
 
   return (
-    <>
+    <div className={styles.container}>
       <Header />
-      <div className={styles.container}>
-        <div className={styles.user_info}>
-          <div>
+      <div className={styles.body}>
+        <div className={styles.user_container}>
+          <div className={styles.user_info}>
             {user?.profileImage ? (
               <img
                 className={styles.user_profile}
@@ -51,24 +51,28 @@ function MyPage() {
                 alt="user-profile"
               />
             )}
+            <div className={styles.user_text}>
+              <h1 className={styles.user_name}>{user ? user.name : "이름"}</h1>
+              <p className={styles.user_email}>{user ? user.email : "Email"}</p>
+            </div>
           </div>
-          <div className={styles.user_text}>
-            <h1>{user ? user.name : "이름"}</h1>
-            <p>{user ? user.email : "Email"}</p>
+          <h4 className={styles.mylists_label}>마이리스트</h4>
+          <div className={styles.mylists}>
+            {myList.map(item => (
+              <MyListBox
+                key={item.directoryId}
+                listName={item.directoryName}
+                listInfo={item.problemList}
+              />
+            ))}
           </div>
         </div>
-        <div className={styles.problem_list}>
-          {myList.map(item => (
-            <MyListBox
-              key={item.directoryId}
-              listName={item.directoryName}
-              listInfo={item.problemList}
-            />
-          ))}
+        <div className={styles.detail_container}>
+          <p>자세히 보시려면 마이리스트를 클릭하세요</p>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
